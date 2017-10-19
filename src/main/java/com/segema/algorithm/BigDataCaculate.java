8031ceacabe9bb6f57b11696effa590644f0d4d3
vangyong <vangyong@126.com>
2017-10-19 20:27:31 +0800

    aaa

---
+++ src/main/java/com/segema/algorithm/BigDataCaculate.java
@@ -0,0 +1,22 @@
+package com.segema.algorithm;
+
+public class BigDataCaculate {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+	
+	static int[] demo(int[] ints,int num) {
+		for(int i=0;i<ints.length;i++) {
+			ints[i]*=num;
+		}
+		
+		for(int i=ints.length-1;i>0;i--) {
+			ints[i-1]+=ints[i]/10;
+			ints[i]=ints[i]%10;
+		}
+		return ints;
+	}
+}

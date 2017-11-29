03f977263255092a104f7b0bbed6649fc6b103fe
vangyong <vangyong@126.com>
2017-11-29 15:39:09 +0800

    增加测试map

--- src/main/java/cn/segema/learn/interview/base/AppMain.java
+++ src/main/java/cn/segema/learn/interview/base/AppMain.java
@@ -1,8 +1,31 @@
 package cn.segema.learn.interview.base;
 
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class AppMain {
-	public static void main(String[] args) {
-		
-	}
+    public static void main(String[] args) {
+
+        Map<String, String> map = new HashMap<String, String>();
+
+        Map map2 = new ConcurrentHashMap();
+
+        Hashtable hashtable = new Hashtable();
+
+
+        Collections.synchronizedMap(map);
+
+    }
+
+    public synchronized void test1() {
+
+    }
+
+    ReentrantLock;
 
 }

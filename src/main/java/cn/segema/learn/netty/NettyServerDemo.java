7f2d1119acd7286f588af26778930689f9ef8606
wangyong <vangyong@126.com>
2019-06-19 17:39:45 +0800

    修改

--- src/main/java/cn/segema/learn/netty/NettyServerDemo.java
+++ src/main/java/cn/segema/learn/netty/NettyServerDemo.java
@@ -1,5 +1,29 @@
 package cn.segema.learn.netty;
 
-public class NettyServerDemo {
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
 
+public class NettyServerDemo {
+	public static void main(String[] args) throws Exception {
+
+		NioEventLoopGroup bossGroup = new NioEventLoopGroup();
+		NioEventLoopGroup workerGroup = new NioEventLoopGroup();
+		ServerBootstrap bootstrap = new ServerBootstrap();
+		bootstrap.group(bossGroup, workerGroup);
+
+		bootstrap.channel(NioServerSocketChannel.class);
+
+		bootstrap.childHandler(new ChannelInitializer<SocketChannel>() {
+			@Override
+			protected void initChannel(SocketChannel ch) throws Exception {
+				ChannelPipeline pipeline = ch.pipeline();
+				// todo: add handler
+			}
+		});
+		bootstrap.bind(8080).sync();
+	}
 }

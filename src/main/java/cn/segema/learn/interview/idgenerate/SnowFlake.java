129d97f23730063bd2a2cd33457a9cf88eec4937
vangyong <vangyong@126.com>
2019-06-30 20:25:02 +0800

    修改

--- src/main/java/cn/segema/learn/interview/idgenerate/SnowFlake.java
+++ src/main/java/cn/segema/learn/interview/idgenerate/SnowFlake.java
@@ -4,14 +4,14 @@ public class SnowFlake {
 	/**
 	 * 起始的时间戳
 	 */
-	private final static long START_STMP = 1480166465631L;
+	private final static long START_STMP = 1288834974657L;
 
 	/**
 	 * 每一部分占用的位数
 	 */
-	private final static long SEQUENCE_BIT = 8; // 序列号占用的位数
-	private final static long MACHINE_BIT = 2; // 机器标识占用的位数
-	private final static long DATACENTER_BIT = 2;// 数据中心占用的位数
+	private final static long SEQUENCE_BIT = 12; // 序列号占用的位数
+	private final static long MACHINE_BIT = 5; // 机器标识占用的位数
+	private final static long DATACENTER_BIT = 5;// 数据中心占用的位数
 
 	/**
 	 * 每一部分的最大值

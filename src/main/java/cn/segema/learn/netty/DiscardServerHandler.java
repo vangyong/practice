1da1ade6cd0a0e21aef9fb3e108aa300075e7c85
wangyong <vangyong@126.com>
2019-08-01 17:40:01 +0800

    修改

---
+++ src/main/java/cn/segema/learn/netty/DiscardServerHandler.java
@@ -0,0 +1,32 @@
+package cn.segema.learn.netty;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.ReferenceCountUtil;
+
+//ChannelInboundHandlerAdapter实现自ChannelInboundHandler
+//ChannelInboundHandler提供了不同的事件处理方法你可以重写
+public class DiscardServerHandler extends ChannelInboundHandlerAdapter {
+
+	@Override
+	public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+		// Discard the received data silently
+		// ByteBuf是一个引用计数对象实现ReferenceCounted，他就是在有对象引用的时候计数+1，无的时候计数-1，当为0对象释放内存
+		ByteBuf in = (ByteBuf) msg;
+		try {
+			while (in.isReadable()) {
+				System.out.println((char) in.readByte());
+				System.out.flush();
+			}
+		} finally {
+			ReferenceCountUtil.release(msg);
+		}
+	}
+
+	@Override
+	public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+		cause.printStackTrace();
+		ctx.close();
+	}
+}

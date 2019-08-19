0f846cc3e1418cfcf464cbaedb1d3e0bcdfcaa47
vangyong <vangyong@126.com>
2019-08-19 23:06:37 +0800

    修改

---
+++ src/main/java/cn/segema/learn/framework/springmvc/SpringMvcDemo.java
@@ -0,0 +1,17 @@
+package cn.segema.learn.framework.springmvc;
+
+public class SpringMvcDemo {
+
+	public static void main(String[] args) {
+//		DispatcherServlet
+//		ModelAndView 
+//		ViewResolver
+		
+//		BeanFactory
+//		AutowiredAnnotationBeanPostProcessor
+//		DefaultListableBeanFactory
+//		AbstractBeanFactory
+//		AbstractAutowireCapableBeanFactory
+	}
+
+}

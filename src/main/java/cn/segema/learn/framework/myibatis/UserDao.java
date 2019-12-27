919fae78380da5486870cbf689ea05b526d16a1b
vangyong <vangyong@126.com>
2019-12-27 22:45:43 +0800

    修改

--- src/main/java/cn/segema/learn/framework/myibatis/UserDao.java
+++ src/main/java/cn/segema/learn/framework/myibatis/UserDao.java
@@ -3,7 +3,7 @@ package cn.segema.learn.framework.myibatis;
 import java.util.List;
 
 public interface UserDao {
-	@ExtInsert(value="insert into user values(#{age},#{name})")
+	@ExtInsert(value="insert into user(age,name) values(#{age},#{name})")
 	public int insertUser(@ExtParam("age") int age,@ExtParam("name") String name);
 	
 	public List<User> selectUserList(User user);

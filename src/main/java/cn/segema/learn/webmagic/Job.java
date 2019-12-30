a6646b9457ab02ec0902a3b07c7fc8fee159bfdc
wangyong <vangyong@126.com>
2019-12-30 14:12:44 +0800

    增加爬虫框架

---
+++ src/main/java/cn/segema/learn/webmagic/Job.java
@@ -0,0 +1,76 @@
+package cn.segema.learn.webmagic;
+
+public class Job {
+	
+	private String companyName;
+	private String companyAddr;
+	private String companyInfo;
+	private String jobName;
+	private String jobAddr;
+	private String jobInfo;
+	private String url;
+	private String salaryMax;
+	private String salaryMin;
+	private String time;
+	public String getCompanyName() {
+		return companyName;
+	}
+	public void setCompanyName(String companyName) {
+		this.companyName = companyName;
+	}
+	public String getCompanyAddr() {
+		return companyAddr;
+	}
+	public void setCompanyAddr(String companyAddr) {
+		this.companyAddr = companyAddr;
+	}
+	public String getCompanyInfo() {
+		return companyInfo;
+	}
+	public void setCompanyInfo(String companyInfo) {
+		this.companyInfo = companyInfo;
+	}
+	public String getJobName() {
+		return jobName;
+	}
+	public void setJobName(String jobName) {
+		this.jobName = jobName;
+	}
+	public String getJobAddr() {
+		return jobAddr;
+	}
+	public void setJobAddr(String jobAddr) {
+		this.jobAddr = jobAddr;
+	}
+	public String getJobInfo() {
+		return jobInfo;
+	}
+	public void setJobInfo(String jobInfo) {
+		this.jobInfo = jobInfo;
+	}
+	public String getUrl() {
+		return url;
+	}
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	public String getSalaryMax() {
+		return salaryMax;
+	}
+	public void setSalaryMax(String salaryMax) {
+		this.salaryMax = salaryMax;
+	}
+	public String getSalaryMin() {
+		return salaryMin;
+	}
+	public void setSalaryMin(String salaryMin) {
+		this.salaryMin = salaryMin;
+	}
+	public String getTime() {
+		return time;
+	}
+	public void setTime(String time) {
+		this.time = time;
+	}
+	
+}

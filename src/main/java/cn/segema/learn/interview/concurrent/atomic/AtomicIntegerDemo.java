bedd9602a15bc570c710eeaa2619df9f99564b10
wangyong <vangyong@126.com>
2020-01-03 17:36:25 +0800

    修改

--- src/main/java/cn/segema/learn/interview/concurrent/atomic/AtomicIntegerDemo.java
+++ src/main/java/cn/segema/learn/interview/concurrent/atomic/AtomicIntegerDemo.java
@@ -1,8 +1,26 @@
 package cn.segema.learn.interview.concurrent.atomic;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class AtomicIntegerDemo {
 
+	private AtomicInteger count = new AtomicInteger();
+
+	public AtomicIntegerDemo(){}
+
+	public int getCount() {
+		return count.get();
+	}
+
+	public void increase() {
+		count.getAndIncrement();
+	}
+
 	public static void main(String[] args) {
+		AtomicIntegerDemo atomicIntegerDemo = new AtomicIntegerDemo();
+		atomicIntegerDemo.getCount();
+		atomicIntegerDemo.increase();
+		System.out.println(atomicIntegerDemo.getCount());
 	}
 
 }

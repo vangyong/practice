7a907f5639e5b0d408a2aebad31bc78a003f16b7
wangyong <vangyong@126.com>
2020-05-07 13:34:40 +0800

    增加rocketmq

--- src/main/java/cn/segema/learn/netty/NettyClientDemo.java
+++ src/main/java/cn/segema/learn/netty/NettyClientDemo.java
@@ -1,45 +1,45 @@
-package cn.segema.learn.netty;
-
-import java.util.Date;
-
-import io.netty.bootstrap.Bootstrap;
-import io.netty.channel.Channel;
-import io.netty.channel.ChannelInitializer;
-import io.netty.channel.ChannelOption;
-import io.netty.channel.nio.NioEventLoopGroup;
-import io.netty.channel.socket.SocketChannel;
-import io.netty.channel.socket.nio.NioSocketChannel;
-import io.netty.handler.codec.DatagramPacketDecoder;
-import io.netty.handler.codec.FixedLengthFrameDecoder;
-import io.netty.handler.codec.string.StringEncoder;
-
-public class NettyClientDemo {
-	public static void main(String[] args) throws InterruptedException {
-        Bootstrap bootstrap = new Bootstrap();
-        NioEventLoopGroup workerGroup = new NioEventLoopGroup();
-
-        bootstrap.group(workerGroup)
-                .channel(NioSocketChannel.class)
-                .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 5000)
-				.option(ChannelOption.SO_KEEPALIVE, true)
-				.option(ChannelOption.TCP_NODELAY, true)
-                .handler(new ChannelInitializer<SocketChannel>() {
-                    @Override
-                    protected void initChannel(SocketChannel ch) {
-                    	ch.pipeline().addLast(new FixedLengthFrameDecoder(31));
-//                        ch.pipeline().addLast(new PacketDecoder());
-//                        ch.pipeline().addLast(new LoginResponseHandler());
-//                        ch.pipeline().addLast(new MessageResponseHandler());
-//                        ch.pipeline().addLast(new StringEncoder());
-                    }
-                });
-
-        Channel channel = bootstrap.connect("localhost", 8080).channel();
-
-        while (true) {
-            channel.writeAndFlush(new Date() + ": hello world!");
-            Thread.sleep(2000);
-            System.out.println("send message end!");
-        }
-    }
-}
+//package cn.segema.learn.netty;
+//
+//import java.util.Date;
+//
+//import io.netty.bootstrap.Bootstrap;
+//import io.netty.channel.Channel;
+//import io.netty.channel.ChannelInitializer;
+//import io.netty.channel.ChannelOption;
+//import io.netty.channel.nio.NioEventLoopGroup;
+//import io.netty.channel.socket.SocketChannel;
+//import io.netty.channel.socket.nio.NioSocketChannel;
+//import io.netty.handler.codec.DatagramPacketDecoder;
+//import io.netty.handler.codec.FixedLengthFrameDecoder;
+//import io.netty.handler.codec.string.StringEncoder;
+//
+//public class NettyClientDemo {
+//	public static void main(String[] args) throws InterruptedException {
+//        Bootstrap bootstrap = new Bootstrap();
+//        NioEventLoopGroup workerGroup = new NioEventLoopGroup();
+//
+//        bootstrap.group(workerGroup)
+//                .channel(NioSocketChannel.class)
+//                .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 5000)
+//				.option(ChannelOption.SO_KEEPALIVE, true)
+//				.option(ChannelOption.TCP_NODELAY, true)
+//                .handler(new ChannelInitializer<SocketChannel>() {
+//                    @Override
+//                    protected void initChannel(SocketChannel ch) {
+//                    	ch.pipeline().addLast(new FixedLengthFrameDecoder(31));
+////                        ch.pipeline().addLast(new PacketDecoder());
+////                        ch.pipeline().addLast(new LoginResponseHandler());
+////                        ch.pipeline().addLast(new MessageResponseHandler());
+////                        ch.pipeline().addLast(new StringEncoder());
+//                    }
+//                });
+//
+//        Channel channel = bootstrap.connect("localhost", 8080).channel();
+//
+//        while (true) {
+//            channel.writeAndFlush(new Date() + ": hello world!");
+//            Thread.sleep(2000);
+//            System.out.println("send message end!");
+//        }
+//    }
+//}
--- src/main/java/cn/segema/learn/rpc/sofa/SofaClientDemo.java
+++ src/main/java/cn/segema/learn/rpc/sofa/SofaClientDemo.java
@@ -1,29 +1,29 @@
-package cn.segema.learn.rpc.sofa;
-
-import com.alipay.sofa.rpc.config.ConsumerConfig;
-import com.alipay.sofa.rpc.config.RegistryConfig;
-
-public class SofaClientDemo {
-	 public static void main(String[] args) {
-	        // 指定注册中心
-	        RegistryConfig registryConfig = new RegistryConfig()
-	                .setProtocol("zookeeper")
-	                .setAddress("127.0.0.1:2181");
-	        // 引用一个服务
-	        ConsumerConfig<HelloService> consumerConfig = new ConsumerConfig<HelloService>()
-	                .setInterfaceId(HelloService.class.getName())
-	                .setProtocol("bolt")
-	                .setRegistry(registryConfig);
-	        // 拿到代理类
-	        HelloService service = consumerConfig.refer();
-	        
-	        // 发起调用
-	        while (true) {
-	            System.out.println(service.sayHello("world"));
-	            try {
-	                Thread.sleep(1000);
-	            } catch (InterruptedException e) {
-	            }
-	        }
-	    }
-}
+//package cn.segema.learn.rpc.sofa;
+//
+//import com.alipay.sofa.rpc.config.ConsumerConfig;
+//import com.alipay.sofa.rpc.config.RegistryConfig;
+//
+//public class SofaClientDemo {
+//	 public static void main(String[] args) {
+//	        // 指定注册中心
+//	        RegistryConfig registryConfig = new RegistryConfig()
+//	                .setProtocol("zookeeper")
+//	                .setAddress("127.0.0.1:2181");
+//	        // 引用一个服务
+//	        ConsumerConfig<HelloService> consumerConfig = new ConsumerConfig<HelloService>()
+//	                .setInterfaceId(HelloService.class.getName())
+//	                .setProtocol("bolt")
+//	                .setRegistry(registryConfig);
+//	        // 拿到代理类
+//	        HelloService service = consumerConfig.refer();
+//	        
+//	        // 发起调用
+//	        while (true) {
+//	            System.out.println(service.sayHello("world"));
+//	            try {
+//	                Thread.sleep(1000);
+//	            } catch (InterruptedException e) {
+//	            }
+//	        }
+//	    }
+//}

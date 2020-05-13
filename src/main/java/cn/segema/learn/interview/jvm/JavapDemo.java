ef1f3c3c52179b57c834abea2aa572450525cb73
wangyong <vangyong@126.com>
2020-05-13 17:23:43 +0800

    modify log4j

---
+++ src/main/java/cn/segema/learn/interview/jvm/JavapDemo.java
@@ -0,0 +1,13 @@
+package cn.segema.learn.interview.jvm;
+
+/**
+* @description 反汇编测试
+* @author wangyong
+* @createDate 2020/05/13
+*/
+public class JavapDemo {
+    public static void main(String[] args) {
+        int i = 0;
+        i++;
+    }
+}

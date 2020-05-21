72ff465b1e9f8af5f0b0cfda02e76230731e51a6
wangyong <vangyong@126.com>
2020-05-21 14:26:50 +0800

    删除测试

--- src/main/java/cn/segema/learn/interview/collection/HashMapDemo.java
+++
@@ -1,9 +0,0 @@
-package cn.segema.learn.interview.collection;
-
-import java.util.HashMap;
-import java.util.Map;
-
-public class HashMapDemo {
-	Map<String, Object> map = new HashMap<String, Object>();
-	map.
-}

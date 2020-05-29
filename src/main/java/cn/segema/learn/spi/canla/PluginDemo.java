4c8c8a5c0e495231d977b6a14e89e9794519f259
wangyong <vangyong@126.com>
2020-05-29 16:55:02 +0800

    修改spi

--- src/main/java/cn/segema/learn/spi/canla/PluginDemo.java
+++ src/main/java/cn/segema/learn/spi/canla/PluginDemo.java
@@ -1,39 +1,38 @@
 package cn.segema.learn.spi.canla;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileReader;
 import java.net.URL;
 import java.net.URLClassLoader;
 
 public class PluginDemo {
-	public static void main(String[] args) {
-		try {
-			// 第一种 配置成文件格式
-			File file = new File("/Users/wangyong/Downloads/plugins/config.txt");
-			BufferedReader in = new BufferedReader(new FileReader(file));
-			String s = new String();
-			while ((s = in.readLine()) != null) {
-				URL url = new URL(s);
-				s = null;
-				URLClassLoader myClassLoader = new URLClassLoader(new URL[] { url },
-						Thread.currentThread().getContextClassLoader());
-				Class<?> myClass = (Class<?>) myClassLoader.loadClass("cn.segema.learn.spi.canla.PluginService");
-				Plugin plugin = (Plugin) myClass.newInstance();
-				String str = plugin.doBusiness("param");
-				System.out.println(str);
+    public static void main(String[] args) {
+        try {
+            // 第一种 配置成文件
+            String filePath = PluginDemo.class.getResource("/").getPath() + "myconfig.properties";
+            BufferedReader in = new BufferedReader(new FileReader(filePath));
+            String s = new String();
+            while ((s = in.readLine()) != null) {
+                URL url = new URL(s);
+                s = null;
+                URLClassLoader myClassLoader =
+                    new URLClassLoader(new URL[] {url}, Thread.currentThread().getContextClassLoader());
+                Class<?> myClass = (Class<?>)myClassLoader.loadClass("cn.segema.learn.spi.canla.PluginService");
+                Plugin plugin = (Plugin)myClass.newInstance();
+                String str = plugin.doBusiness("param");
+                System.out.println(str);
+            }
 
-				// 第二种
-				URL url1 = new URL("file:/Users/wangyong/Downloads/plugins/pluginservice.jar");
-				URLClassLoader myClassLoader1 = new URLClassLoader(new URL[] { url1 },
-						Thread.currentThread().getContextClassLoader());
-				Class<?> myClass1 = myClassLoader1.loadClass("cn.segema.learn.spi.canla.PluginService");
-				Plugin plugin1 = (Plugin) myClass1.newInstance();
-				String str1 = plugin1.doBusiness("param1");
-				System.out.println(str1);
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+            // 第二种 直接读取
+            URL url1 = new URL("file:E:\\data\\spi\\pluginservice.jar");
+            URLClassLoader myClassLoader1 =
+                new URLClassLoader(new URL[] {url1}, Thread.currentThread().getContextClassLoader());
+            Class<?> myClass1 = myClassLoader1.loadClass("cn.segema.learn.spi.canla.PluginService");
+            Plugin plugin1 = (Plugin)myClass1.newInstance();
+            String str1 = plugin1.doBusiness("param1");
+            System.out.println(str1);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
